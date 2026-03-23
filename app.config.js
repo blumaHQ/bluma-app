cd11f9b137f9a60e8e0f58e9af126fe57cf1459a
Maribel Ferreira <maribelferreira@users.noreply.github.com>
2026-03-23 10:36:48 +0000

    Update version to 1.4.0 in app.config.js, package.json, and package-lock.json for release

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ export default {
   expo: {
     name: getAppName(),
     slug: "bluma",
-    version: "1.3.0",
+    version: "1.4.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "bluma",
@@ -27,7 +27,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 6,
+      versionCode: 7,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#FFFFFF",
